23ea53b66eebdbfbad9e44f213376a0e904d18f7
CJINBNH <alex_egorov_89@mail.ru>
2022-08-22 16:53:35 +0400

    Completed task_028

--- task_028(additionally)/Program.cs
+++ task_028(additionally)/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, в которой задается массив чисел(ввод может быть с клавиатуры, либо можете сами задавать массив чисел)
+//и выдаёт произведение чисел от 1 до N, каждого элемента этого массива.
+
+Console.Clear();
+int[] array = new int[3];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 10);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+int[] newArray = new int[3];
+for (int j = 0; j < array.Length; j++)
+{
+    int n = array[j];
+    newArray[j] = Factorial(n);
+    int Factorial(int n)
+    {
+        if (n == 1 || n == 0) return 1;
+
+        return n * Factorial(n - 1);
+    }
+}
+Console.WriteLine('[' + string.Join(", ", newArray) + ']');

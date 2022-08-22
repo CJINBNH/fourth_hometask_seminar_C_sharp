12b4e490e90eeb3b4acdf35e08a11c3aa69a4ef1
CJINBNH <alex_egorov_89@mail.ru>
2022-08-22 15:30:00 +0400

    Completed task_029

--- task_029/Program.cs
+++ task_029/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+int[] array = new int[8];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 10);
+}
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
